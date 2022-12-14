b9545f5584b00d2358ff51314486407da2532a0a
Ahmet Barış Durak <80640352+ahmetbarisdurak@users.noreply.github.com>
2022-12-14 22:22:32 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
 import random
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+def getCountry(tableName, countryIndex):
+    conn = get_db_connection()
+    country = conn.execute('SELECT * FROM ? WHERE id = ?',
+                        (tableName, countryIndex)).fetchone()
+    conn.close()
+    if country is None:
+        abort(404)
+    return country
+
 @app.route('/')
 def homePage():
-    return render_template("base.html")
+    conn = get_db_connection()
+    countries = conn.execute('SELECT * FROM generalInfo').fetchall()
+    conn.close()
+    
+    return render_template("base.html", countries=countries)
 
 @app.route('/info')
 def countryInformation():
@@ -29,19 +42,61 @@ def showInfo():
     inputData = list(request.form.values())
     # Burada datayı rahat bir şekilde alabiliyorum.
     conn = get_db_connection()
-    countries = conn.execute('SELECT * \
-                            FROM generalInfo JOIN countryPopulations \
-                            ON generalInfo.countryIndex = countryPopulations.countryIndex \
-                                  ').fetchall()
+    #countries = conn.execute('SELECT * \
+                            #FROM generalInfo JOIN countryPopulations \
+                            #ON generalInfo.countryIndex = countryPopulations.countryIndex \
+                             #     ').fetchall()
+    
+    countryInfo = conn.execute('SELECT * FROM generalInfo').fetchall()
+    countryPopulations = conn.execute('SELECT * FROM countryPopulations').fetchall()
+
     conn.close()
 
-    for country in countries:
+    for country in countryInfo:
         if(inputData[0] == country['countryName']):
-            country1 = country
+            countryInfo = country
+            countryIndex = country['countryIndex']
 
-    print(country1.keys())
+    for country in countryPopulations:
+        if(countryIndex == country['countryIndex']):
+            countryPopulations = country
 
-    return render_template('showInfo.html', country=country1)
+    return render_template('showInfo.html', countryInfo=countryInfo,
+        countryPopulations=countryPopulations)
+
+@app.route('/listCountries')
+def listCountries():
+    conn = get_db_connection()
+    countries = conn.execute('SELECT * FROM generalInfo').fetchall()
+    conn.close()
+    
+    return render_template("listCountries.html", countries=countries) 
+
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    country = getCountry(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+
+        elif not content:
+            flash('Content is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
 @app.route('/comparison')
 def countryComparator():
     conn = get_db_connection()
@@ -78,17 +133,28 @@ def currencyExchange():
 @app.route('/randomInfo')
 def randomInfo():
     conn = get_db_connection()
-    countries = conn.execute('SELECT * FROM generalInfo').fetchall()
+    countries = conn.execute('SELECT * \
+                            FROM generalInfo JOIN countryPopulations \
+                            ON generalInfo.countryIndex = countryPopulations.countryIndex \
+                                 ').fetchall()
     conn.close()
 
     
+    
     randomCountry = random.randint(0,len(countries) - 1)
     randomIndex = random.randint(0, len(countries[0]) - 1)
-    
-    print(" random country is (?)", randomCountry)
-    print("random index is (?)", randomIndex)
 
-    print(countries[randomCountry].keys())
+    while(countries[randomCountry].keys()[randomIndex] == "countryName" or \
+     countries[randomCountry].keys()[randomIndex] == "countryIndex" ):
+
+        randomIndex = random.randint(0, len(countries[0]) - 1)
+
+        print(" random country is (?)", randomCountry)
+        print("random index is (?)", randomIndex)
+
+        print(countries[randomCountry].keys())
+
+
 
     text = "Did you know that " + (countries[randomCountry].keys())[randomIndex]+ " of " 
     text = text + countries[randomCountry]["countryName"]  
@@ -99,3 +165,31 @@ def randomInfo():
 @app.route('/ranking')
 def countryRanking():
     print("country ranking")
+
+@app.route("/delete", methods=['GET', 'POST'])
+def delete():
+    inputData = list(request.form.getlist('countryDeletion'))
+
+    print(inputData)
+    
+    willDelete = []
+
+    conn = get_db_connection()
+    countries = conn.execute('SELECT * FROM generalInfo').fetchall()
+    conn.commit()
+    
+    for x in inputData:
+        for country in countries:
+            if(country['countryName'] == x):  
+                willDelete.append(country['countryIndex'])
+
+
+    print(willDelete)
+
+    for i in willDelete:
+        countries = conn.execute('DELETE FROM generalInfo WHERE countryIndex = ?', (i,))
+        conn.commit()
+
+    conn.close()
+
+    return redirect(url_for('listCountries'))
